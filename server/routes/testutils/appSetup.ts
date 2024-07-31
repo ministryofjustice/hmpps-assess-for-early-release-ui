87a3acf0de9f35eecbcc0000aeab8f58ff3aa5dc
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-07-31 11:37:53 +0100

    Initial commit

---
+++ server/routes/testutils/appSetup.ts
@@ -0,0 +1,70 @@
+import express, { Express } from 'express'
+import { NotFound } from 'http-errors'
+import { v4 as uuidv4 } from 'uuid'
+
+import routes from '../index'
+import nunjucksSetup from '../../utils/nunjucksSetup'
+import errorHandler from '../../errorHandler'
+import * as auth from '../../authentication/auth'
+import type { Services } from '../../services'
+import AuditService from '../../services/auditService'
+import { HmppsUser } from '../../interfaces/hmppsUser'
+import setUpWebSession from '../../middleware/setUpWebSession'
+
+jest.mock('../../services/auditService')
+
+export const user: HmppsUser = {
+  name: 'FIRST LAST',
+  userId: 'id',
+  token: 'token',
+  username: 'user1',
+  displayName: 'First Last',
+  authSource: 'nomis',
+  staffId: 1234,
+  userRoles: [],
+}
+
+export const flashProvider = jest.fn()
+
+function appSetup(services: Services, production: boolean, userSupplier: () => HmppsUser): Express {
+  const app = express()
+
+  app.set('view engine', 'njk')
+
+  nunjucksSetup(app)
+  app.use(setUpWebSession())
+  app.use((req, res, next) => {
+    req.user = userSupplier() as Express.User
+    req.flash = flashProvider
+    res.locals = {
+      user: { ...req.user } as HmppsUser,
+    }
+    next()
+  })
+  app.use((req, res, next) => {
+    req.id = uuidv4()
+    next()
+  })
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(routes(services))
+  app.use((req, res, next) => next(new NotFound()))
+  app.use(errorHandler(production))
+
+  return app
+}
+
+export function appWithAllRoutes({
+  production = false,
+  services = {
+    auditService: new AuditService(null) as jest.Mocked<AuditService>,
+  },
+  userSupplier = () => user,
+}: {
+  production?: boolean
+  services?: Partial<Services>
+  userSupplier?: () => HmppsUser
+}): Express {
+  auth.default.authenticationMiddleware = () => (req, res, next) => next()
+  return appSetup(services as Services, production, userSupplier)
+}
