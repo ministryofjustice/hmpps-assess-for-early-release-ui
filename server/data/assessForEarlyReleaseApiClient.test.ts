3cdf8f22b22cd2adf50c2f2badd1dba45fef2225
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2024-09-05 12:25:47 +0100

    Initial APi Client Test

---
+++ server/data/assessForEarlyReleaseApiClient.test.ts
@@ -0,0 +1,40 @@
+import nock from 'nock'
+import config from '../config'
+import { AssessForEarlyReleaseApiClient } from '.'
+import { OffenderSummary } from '../@types/assessForEarlyReleaseApiClientTypes'
+import { createOffenderSummary } from './testutils/testObjects'
+import { mockRequest } from '../routes/testutils/requestTestUtils'
+
+describe('feComponentsClient', () => {
+  let fakeComponentsApi: nock.Scope
+  let assessForEarlyReleaseApiClient: AssessForEarlyReleaseApiClient
+  const offenderSummaryList = [createOffenderSummary({})]
+  const req = mockRequest({})
+
+  beforeEach(() => {
+    fakeComponentsApi = nock(config.apis.assessForEarlyReleaseApi.url)
+    assessForEarlyReleaseApiClient = new AssessForEarlyReleaseApiClient(req.middleware.clientToken)
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+    nock.cleanAll()
+  })
+
+  describe('getComponents', () => {
+    it('should return data from api', async () => {
+      const prisonCode = 'MDI'
+      const response: { data: OffenderSummary[] } = {
+        data: offenderSummaryList,
+      }
+
+      fakeComponentsApi
+        .get(`/prison/${prisonCode}/case-admin/caseload`)
+        .matchHeader('authorization', `Bearer ${req.middleware.clientToken}`)
+        .reply(200, response)
+
+      const output = await assessForEarlyReleaseApiClient.getCaseAdminCaseload(prisonCode)
+      expect(output).toEqual(response)
+    })
+  })
+})
