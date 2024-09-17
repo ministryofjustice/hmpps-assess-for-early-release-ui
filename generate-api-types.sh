57079e99e51bc84268faaf608ca48e27703fc23d
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2024-09-17 09:17:49 +0100

    CVSL-2157 Update types (#18)

---
+++ generate-api-types.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+OUTPUT_FILE=server/@types/assessForEarlyReleaseApiImport/index.d.ts
+SWAGGER_ENDPOINT=/v3/api-docs
+
+if [[ $1 == "--local" ]]; then
+  npx openapi-typescript "http://localhost:8089$SWAGGER_ENDPOINT" > "$OUTPUT_FILE"
+else
+  npx openapi-typescript "https://assess-for-early-release-dev.hmpps.service.justice.gov.uk$SWAGGER_ENDPOINT" > "$OUTPUT_FILE"
+fi
