0d2de2246a8b52a826e41f53bb1f2a241f7213f4
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2024-09-20 12:44:08 +0100

    CVSL-2124: Update to use new static paths.

--- server/routes/optout/index.ts
+++ server/routes/optout/index.ts
@@ -1,30 +1,29 @@
 import { RequestHandler, Router } from 'express'
-import { path } from 'static-path'
+import { Path } from 'static-path'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import roleCheckMiddleware from '../../middleware/roleCheckMiddleware'
 import AuthRole from '../../enumeration/authRole'
 import OptOutCheckRoutes from './optOutCheck'
 import { Services } from '../../services'
 import OptOutRoutes from './optOut'
+import paths from '../paths'
 
 export default function Index({ caseAdminCaseloadService, optOutService }: Services): Router {
   const router = Router()
-  const optOutCheckPath = path('/prison/assessment/:prisonNumber/opt-out-check')
-  const optOutPath = path('/prison/assessment/:prisonNumber/opt-out')
 
-  const get = (routerPath: string, handler: RequestHandler) =>
-    router.get(routerPath, roleCheckMiddleware([AuthRole.SUPPORT]), asyncMiddleware(handler))
+  const get = <T extends string>(routerPath: Path<T>, handler: RequestHandler) =>
+    router.get(routerPath.pattern, roleCheckMiddleware([AuthRole.CASE_ADMIN]), asyncMiddleware(handler))
 
-  const post = (routerPath: string, handler: RequestHandler) =>
-    router.post(routerPath, roleCheckMiddleware([AuthRole.SUPPORT]), asyncMiddleware(handler))
+  const post = <T extends string>(routerPath: Path<T>, handler: RequestHandler) =>
+    router.post(routerPath.pattern, roleCheckMiddleware([AuthRole.SUPPORT]), asyncMiddleware(handler))
 
   const optOutCheckHandler = new OptOutCheckRoutes(caseAdminCaseloadService)
-  get(optOutCheckPath.pattern, optOutCheckHandler.GET)
-  post(optOutCheckPath.pattern, optOutCheckHandler.POST)
+  get(paths.prison.assessment.optOutCheckPath, optOutCheckHandler.GET)
+  post(paths.prison.assessment.optOutCheckPath, optOutCheckHandler.POST)
 
   const optOutHandler = new OptOutRoutes(caseAdminCaseloadService, optOutService)
-  get(optOutPath.pattern, optOutHandler.GET)
-  post(optOutPath.pattern, optOutHandler.POST)
+  get(paths.prison.assessment.optOutPath, optOutHandler.GET)
+  post(paths.prison.assessment.optOutPath, optOutHandler.POST)
 
   return router
 }
