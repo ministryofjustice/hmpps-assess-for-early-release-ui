bb1ceecfe955eff3c57afca7142537e545dde579
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2024-12-06 10:59:09 +0000

    CVSL-2264-AFER Address Checks persist additional information for standard address checks

--- server/routes/curfewAddress/moreInfoRequiredCheck.ts
+++ server/routes/curfewAddress/moreInfoRequiredCheck.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from 'express'
-import { CaseAdminCaseloadService } from '../../services'
+import { AddressService, CaseAdminCaseloadService } from '../../services'
 import { convertToTitleCase } from '../../utils/utils'
 import { validateRequest } from '../../middleware/setUpValidationMiddleware'
 import paths from '../paths'
+import { UpdateCaseAdminAdditionInfoRequest } from '../../@types/assessForEarlyReleaseApiClientTypes'
 
 export default class MoreInfoRequiredCheckRoutes {
-  constructor(private readonly caseAdminCaseloadService: CaseAdminCaseloadService) {}
+  constructor(
+    private readonly caseAdminCaseloadService: CaseAdminCaseloadService,
+    private readonly addressService: AddressService,
+  ) {}
 
   GET = async (req: Request, res: Response): Promise<void> => {
     const assessmentSummary = await this.caseAdminCaseloadService.getAssessmentSummary(
@@ -26,18 +30,23 @@ export default class MoreInfoRequiredCheckRoutes {
       return body.moreInfoRequiredCheck ? [] : [{ field: 'moreInfoRequiredCheck', message: validationMessage }]
     })
 
-    if (req.body.moreInfoRequiredCheck === 'no') {
-      return res.redirect(
-        paths.prison.assessment.curfewAddress.requestMoreAddressChecks({
-          prisonNumber: req.params.prisonNumber,
-        }),
+    const { prisonNumber } = req.params
+    const requestId = req.params.checkRequestId
+
+    if (req.body.moreInfoRequiredCheck === 'yes') {
+      await this.addressService.updateCaseAdminAdditionalInformation(
+        req?.middleware?.clientToken,
+        prisonNumber,
+        Number(requestId),
+        {
+          additionalInformation: req.body.addMoreInfo,
+        } as UpdateCaseAdminAdditionInfoRequest,
       )
     }
 
     return res.redirect(
-      paths.prison.assessment.curfewAddress.moreInformationRequired({
-        prisonNumber: req.params.prisonNumber,
-        checkRequestId: req.params.checkRequestId,
+      paths.prison.assessment.curfewAddress.requestMoreAddressChecks({
+        prisonNumber,
       }),
     )
   }
