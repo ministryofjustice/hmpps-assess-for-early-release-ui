ad86442079729fd24a19174298a09703149acabe
nishanthmahasamudram <nishanth.mahasamudram@digital.justice.gov.uk>
2024-12-06 15:30:56 +0000

    int tests

---
+++ integration_tests/caseAdmin/moreInfoRequiredCheck.spec.ts
@@ -0,0 +1,73 @@
+import { expect, test } from '@playwright/test'
+import { assessmentSummary, createCheckRequestsForAssessmentSummary } from '../mockApis/assessForEarlyReleaseData'
+import assessForEarlyRelease from '../mockApis/assessForEarlyRelease'
+
+import { login, resetStubs } from '../testUtils'
+import paths from '../../server/routes/paths'
+import playwrightConfig from '../../playwright.config'
+
+test.describe('Can add more information for address checks', () => {
+  test.afterEach(async () => {
+    await resetStubs()
+  })
+  test('Should display add more info text box, if user selects option yes', async ({ page }) => {
+    const prisonNumber = 'A1234AE'
+    const checkRequestId = createCheckRequestsForAssessmentSummary[0].requestId
+
+    await assessForEarlyRelease.stubGetAssessmentSummary(assessmentSummary(prisonNumber))
+    await assessForEarlyRelease.stubGetCheckRequestsForAssessment(prisonNumber, [
+      createCheckRequestsForAssessmentSummary[0],
+    ])
+    await assessForEarlyRelease.stubGetUpdateCaseAdminAdditionalInformation(prisonNumber, checkRequestId)
+
+    await login(page, { authorities: ['ROLE_LICENCE_CA'] })
+
+    await page.goto(
+      paths.prison.assessment.curfewAddress.moreInformationRequiredCheck({ prisonNumber, checkRequestId }),
+    )
+
+    await expect(page).toHaveURL(
+      `${playwrightConfig.use.baseURL}/prison/assessment/${prisonNumber}/curfew-address/${checkRequestId}/more-information-required-check`,
+    )
+
+    await expect(page.getByRole('heading', { name: 'Do you need to enter any more' })).toBeVisible()
+    await page.getByLabel('Yes').click()
+    await expect(page.getByLabel('Add more information')).toBeVisible()
+    await page.getByLabel('Add more information').fill('more info')
+    await page.getByRole('button', { name: 'Continue' }).click()
+
+    await expect(page).toHaveURL(
+      `${playwrightConfig.use.baseURL}/prison/assessment/${prisonNumber}/curfew-address/request-more-address-checks`,
+    )
+  })
+
+  test('Should not display add more info text box, if user selects option no', async ({ page }) => {
+    const prisonNumber = 'A1234AE'
+    const checkRequestId = createCheckRequestsForAssessmentSummary[0].requestId
+
+    await assessForEarlyRelease.stubGetAssessmentSummary(assessmentSummary(prisonNumber))
+    await assessForEarlyRelease.stubGetCheckRequestsForAssessment(prisonNumber, [
+      createCheckRequestsForAssessmentSummary[0],
+    ])
+    await assessForEarlyRelease.stubGetUpdateCaseAdminAdditionalInformation(prisonNumber, checkRequestId)
+
+    await login(page, { authorities: ['ROLE_LICENCE_CA'] })
+
+    await page.goto(
+      paths.prison.assessment.curfewAddress.moreInformationRequiredCheck({ prisonNumber, checkRequestId }),
+    )
+
+    await expect(page).toHaveURL(
+      `${playwrightConfig.use.baseURL}/prison/assessment/${prisonNumber}/curfew-address/${checkRequestId}/more-information-required-check`,
+    )
+
+    await expect(page.getByRole('heading', { name: 'Do you need to enter any more' })).toBeVisible()
+    await page.getByLabel('No').click()
+    await expect(page.getByLabel('Add more information')).not.toBeVisible()
+    await page.getByRole('button', { name: 'Continue' }).click()
+
+    await expect(page).toHaveURL(
+      `${playwrightConfig.use.baseURL}/prison/assessment/${prisonNumber}/curfew-address/request-more-address-checks`,
+    )
+  })
+})
