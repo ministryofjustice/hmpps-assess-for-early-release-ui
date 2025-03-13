320d6ecbbdb5ea9e00b785401e756de0334ca214
AledWEvans <aled.evans1@justice.gov.uk>
2025-03-13 15:54:16 +0000

    CVSL-2584 AFER | Preparation for displaying - update after review comments by nish

--- server/routes/form/assessmentForm.ts
+++ server/routes/form/assessmentForm.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import moment from 'moment'
 import FormService from '../../services/formService'
 import { Agent, DocumentSubjectType } from '../../@types/assessForEarlyReleaseApiClientTypes'
+import { getValidDocumentSubjectType } from '../../utils/utils'
 
 export default class AssessmentFormRoutes {
   constructor(private readonly formService: FormService) {}
@@ -9,7 +10,7 @@ export default class AssessmentFormRoutes {
   GET = async (req: Request, res: Response): Promise<void> => {
     const agent = res.locals.agent as Agent
     const { prisonNumber } = req.params
-    const documentSubjectType = this.getValidDocumentSubjectType(req)
+    const documentSubjectType = getValidDocumentSubjectType(req.params.documentSubjectType)
     const pageData = await this.formService.getForm(
       req?.middleware?.clientToken,
       agent,
@@ -22,28 +23,5 @@ export default class AssessmentFormRoutes {
   private getDownloadFileName(prisonNumber: string, documentSubjectType: DocumentSubjectType) {
     const today = moment().format('DD-MM-YY')
     return `${prisonNumber}_${documentSubjectType}_${today}.pdf`
-  }
-
-  private getValidDocumentSubjectType(req: Request) {
-    const { documentSubjectType } = req.params
-
-    switch (documentSubjectType) {
-      case 'OFFENDER_ELIGIBLE_FORM':
-      case 'OFFENDER_ADDRESS_CHECKS_INFORMATION_FORM':
-      case 'OFFENDER_ADDRESS_CHECKS_FORM':
-      case 'OFFENDER_OPT_OUT_FORM':
-      case 'OFFENDER_NOT_ELIGIBLE_FORM':
-      case 'OFFENDER_NOT_SUITABLE_FORM':
-      case 'OFFENDER_ADDRESS_UNSUITABLE_FORM':
-      case 'OFFENDER_POSTPONED_FORM':
-      case 'OFFENDER_NOT_ENOUGH_TIME_FORM':
-      case 'OFFENDER_APPROVED_FORM':
-      case 'OFFENDER_AGENCY_NOTIFICATION_FORM':
-      case 'OFFENDER_CANCEL_AGENCY_NOTIFICATION_FORM':
-      case 'OFFENDER_REFUSED_FORM':
-        return documentSubjectType as DocumentSubjectType
-      default:
-        throw new Error(`Unknown document type : ${documentSubjectType}`)
-    }
   }
 }
